093417b985062f94c64fba403c51f9be50827bc1
matboe-94 <matthias.boeker@digdir.no>
2025-07-23 17:45:14 +0200

    import download script

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import subprocess
 import logging
 from pathlib import Path
 
-from get_initiell_skjema import run as download_skjema
+from dynamic_scripts.get_initiell_skjema import run as download_skjema
 from upload_skjema import run as upload_skjema
 
 app = Flask(__name__)
@@ -34,7 +34,7 @@ def handle_event():
                 "app_name": app_name
             }, 500
         return "Event received", 200
-
+        
     except Exception as e:
         logging.error(f"Error: {e}")
         return f"Internal Server Error: {str(e)}", 500
