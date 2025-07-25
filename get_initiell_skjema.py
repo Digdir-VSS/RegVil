bb4cf87a1891b5862f3e898d5d9678f366b8c8ca
matboe-94 <matthias.boeker@digdir.no>
2025-07-25 15:22:04 +0200

    refactor to new tracker

--- get_initiell_skjema.py
+++ get_initiell_skjema.py
@@ -14,10 +14,13 @@ from config.config_loader import load_full_config
 load_dotenv()
 
 def is_valid_instance(meta_data: dict, tag_expected: str) -> bool:
-    return (
-        meta_data.get("tags") == [tag_expected] and
-        meta_data.get("createdBy") != meta_data.get("lastChangedBy")
-    )
+    if meta_data:
+        return (
+            meta_data.get("tags") == [tag_expected] and
+            meta_data.get("createdBy") != meta_data.get("lastChangedBy")
+        )
+    else:
+        False
 
 def write_to_json(data: Dict[str, Any], path_to_folder: Path, filename: str) -> None:
     with open(path_to_folder / filename, 'w', encoding='utf-8') as file:
@@ -30,14 +33,14 @@ secret_value = secret.value
 
 def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
     path_to_config_folder = Path(__file__).parent / "config_files"
-    config = load_full_config(path_to_config_folder, "regvil-2025-initiell", os.getenv("ENV"))
+    config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
 
 
     regvil_instance_client = AltinnInstanceClient.init_from_config(config)
     tracker = InstanceTracker.from_directory(f"{os.getenv("ENV")}/event_log/")
 
 
-    digitaliseringstiltak_report_id = get_reportid_from_blob(f"{os.getenv("ENV")}/event_log/","regvil-2025-initiell", "0a532b55-76b3-41f9-9b3c-58eee9eaea6f", config.app_config.tag["tag_instance"])
+    digitaliseringstiltak_report_id = get_reportid_from_blob(f"{os.getenv("ENV")}/event_log/",app_name, instance_id, config.app_config.tag["tag_instance"])
 
 
 
@@ -48,13 +51,13 @@ def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
         return None
     try:
         instance_meta_info = instance_meta.json()
-        meta_data = get_meta_data_info(instance_meta_info["data"])
+        meta_data = get_meta_data_info(instance_meta_info.get("data"))
 
         if is_valid_instance(meta_data, config.app_config.tag["tag_instance"]):
             instance_data = regvil_instance_client.get_instance_data(
                     party_id,
                     instance_id,
-                    meta_data["id"]
+                    meta_data.get("id")
                 )
 
             report_data = instance_data.json()           
@@ -62,7 +65,7 @@ def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
             response = regvil_instance_client.tag_instance_data(
                     party_id,
                     instance_id,
-                    meta_data["id"],
+                    meta_data.get("id"),
                     config.app_config.tag["tag_download"]
                 )
 
@@ -75,11 +78,11 @@ def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
                     config.app_config.tag["tag_download"]
                 )
             tracker.save_to_disk()
-            logging.info(f"Successfully downloaded and tagged: {filename} (HTTP {response.status_code})")
-            return {"org_number": instance_meta_info["instanceOwner"]["organisationNumber"], "report_id": pending_instance["digitaliseringstiltak_report_id"] ,"dato": config.app_config.get_date(report_data), "app_name": config.workflow_dag.get_next(app_name), "prefill_data": report_data} #Write logic to get dato out of download
+            logging.info(f"Successfully downloaded and tagged: {instance_id} (HTTP {response.status_code})")
+            return {"org_number": instance_meta_info["instanceOwner"]["organisationNumber"], "report_id": digitaliseringstiltak_report_id ,"dato": config.app_config.get_date(report_data), "app_name": config.workflow_dag.get_next(app_name), "prefill_data": report_data} #Write logic to get dato out of download
 
         else:
-            logging.warning(f"Already downloaded or invalid tags for: {pending_instance['digitaliseringstiltak_report_id']} - {pending_instance['instanceId']}")
+            logging.warning(f"Already downloaded or invalid tags for: {digitaliseringstiltak_report_id} - {instance_id}")
             return None
     except Exception as e:
             logging.exception(f"Error processing party id: {party_id}, instance id {instance_id}")
