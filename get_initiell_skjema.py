0b3e02ede2c8e23806059ccd51bd5be7c9cf4488
matboe-94 <matthias.boeker@digdir.no>
2025-07-28 14:49:18 +0200

    return http response to app.py

--- get_initiell_skjema.py
+++ get_initiell_skjema.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 from pathlib import Path
@@ -6,6 +6,7 @@ import logging
 import json
 from dotenv import load_dotenv
 import os
+import azure.functions as func
 
 from clients.instance_client import AltinnInstanceClient, get_meta_data_info
 from clients.instance_logging import InstanceTracker, get_reportid_from_blob
@@ -19,7 +20,7 @@ def is_valid_instance(meta_data: dict) -> bool:
             meta_data.get("createdBy") != meta_data.get("lastChangedBy")
         )
     else:
-        False
+        return False
 
 def write_to_json(data: Dict[str, Any], path_to_folder: Path, filename: str) -> None:
     with open(path_to_folder / filename, 'w', encoding='utf-8') as file:
@@ -30,7 +31,7 @@ client = SecretClient(vault_url=os.getenv("MASKINPORTEN_SECRET_VAULT_URL"), cred
 secret = client.get_secret(os.getenv("MASKINPORTEN_SECRET_NAME"))
 secret_value = secret.value
 
-def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
+def run(party_id: str, instance_id: str, app_name: str) -> Tuple[Dict[str, str], func.HttpResponse]:
     path_to_config_folder = Path(__file__).parent / "config_files"
     config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
 
@@ -44,22 +45,28 @@ def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
     instance_meta = regvil_instance_client.get_instance(party_id, instance_id)
 
     if not instance_meta:
-        logging.error(f"Failed to fetch instance: {instance_meta.status_code if instance_meta else 'No response'}")
-        return None
+        logging.error(f"Failed to fetch party id: {party_id} instance id: {instance_id}")
+        return {}, func.HttpResponse("Failed to fetch instance from Altinn", status_code=502)
+    if instance_meta.status_code not in [200, 201]:
+        return {}, func.HttpResponse(instance_meta.text, status_code=instance_meta.status_code)
+
     try:
         instance_meta_info = instance_meta.json()
-        meta_data = get_meta_data_info(instance_meta_info.get("data"))
+    except ValueError:
+        logging.error(f"Error processing party id: {party_id}, instance id {instance_id}")
+        return {}, func.HttpResponse("Invalid instance data", status_code=502)
+    meta_data = get_meta_data_info(instance_meta_info.get("data"))
 
-        if is_valid_instance(meta_data, config.app_config.tag["tag_instance"]):
-            instance_data = regvil_instance_client.get_instance_data(
+    if is_valid_instance(meta_data, config.app_config.tag["tag_instance"]):
+        instance_data = regvil_instance_client.get_instance_data(
                     party_id,
                     instance_id,
                     meta_data.get("id")
                 )
 
-            report_data = instance_data.json()           
+        report_data = instance_data.json()           
 
-            tracker.logging_instance(
+        tracker.logging_instance(
                     instance_id,
                     instance_meta_info["instanceOwner"]["organisationNumber"],
                     digitaliseringstiltak_report_id,
@@ -68,14 +75,11 @@ def run(party_id: str, instance_id: str, app_name: str) -> Dict[str, str]:
                     config.app_config.tag["tag_download"]
                 )
             
-            tracker.save_to_disk()
+        tracker.save_to_disk()
             
-            logging.info(f"Successfully downloaded: OrgNumber {instance_meta_info['instanceOwner']['organisationNumber']} InstanceId: {instance_id}) DigireportId: {digitaliseringstiltak_report_id}")
-            return {"org_number": instance_meta_info["instanceOwner"]["organisationNumber"], "digitaliseringstiltak_report_id": digitaliseringstiltak_report_id ,"dato": config.app_config.get_date(report_data), "app_name": config.workflow_dag.get_next(app_name), "prefill_data": report_data}
+        logging.info(f"Successfully downloaded: OrgNumber {instance_meta_info['instanceOwner']['organisationNumber']} App name: {app_name} InstanceId: {instance_id}) DigireportId: {digitaliseringstiltak_report_id}")
+        return {"org_number": instance_meta_info["instanceOwner"]["organisationNumber"], "digitaliseringstiltak_report_id": digitaliseringstiltak_report_id ,"dato": config.app_config.get_date(report_data), "app_name": config.workflow_dag.get_next(app_name), "prefill_data": report_data}, func.HttpResponse("Instance downloaded", status_code=200)
 
-        else:
-            logging.warning(f"Already downloaded or invalid tags for: {app_name} - {digitaliseringstiltak_report_id} - {instance_id}")
-            return None
-    except Exception as e:
-            logging.exception(f"Error processing party id: {party_id}, instance id {instance_id}")
-            raise
+    else:
+        logging.warning(f"Already downloaded: {app_name} InstanceId: {instance_id} ReportId: {digitaliseringstiltak_report_id}")
+        return {}, func.HttpResponse("Instance already downloaded", status_code=204)
