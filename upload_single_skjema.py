f02fa2a32dd489adbda3233604fbc090bfaf8034
matboe-94 <matthias.boeker@digdir.no>
2025-07-28 11:53:13 +0200

    fix-instance-id-fix

--- upload_single_skjema.py
+++ upload_single_skjema.py
@@ -21,15 +21,19 @@ client = SecretClient(
 secret = client.get_secret(os.getenv("MASKINPORTEN_SECRET_NAME"))
 secret_value = secret.value
 
-def create_payload(org_number: str, dato: str, app_config: APIConfig, prefill_data: DataModel) -> Dict[str, Tuple[str, str, str]]:
+def split_party_instance_id(party_instance_id: str) -> Tuple[str]:
+     party_id, instance_id = party_instance_id.split("/")
+     return party_id, instance_id
+
+def create_payload(org_number: str, dato: str, api_config: APIConfig, prefill_data: DataModel) -> Dict[str, Tuple[str, str, str]]:
     instance_data = {
-            "appId": f"digdir/{app_config.app_config.app_name}",
+            "appId": f"digdir/{api_config.app_config.app_name}",
             "instanceOwner": {
                 "personNumber": None,
                 "organisationNumber": org_number,
             },
-            "dueBefore": dato,
-            "visibleAfter": None,
+            "dueBefore": None,
+            "visibleAfter": dato,
         }
     files = {
             "instance": (
@@ -51,62 +55,62 @@ def load_in_json(path_to_json_file: Path) -> Any:
         return json.load(file)
 
 
-def run(org_number: str, report_id: str, dato: str, app_name: str, prefill_data: DataModel) -> None:
+def run(org_number: str, digitaliseringstiltak_report_id: str, dato: str, app_name: str, prefill_data: DataModel) -> None:
 
     logging.info("Starting Altinn survey sending instance processing")
     path_to_config_folder = Path(__file__).parent / "config_files"
-    app_config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
+    api_config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
 
     regvil_instance_client = AltinnInstanceClient.init_from_config(
-        app_config,
+        api_config,
     )
 
-    tracker = InstanceTracker.from_directory(f"{os.getenv("ENV")}/event_log/")
+    tracker = InstanceTracker.from_directory(f"{os.getenv('ENV')}/event_log/")
 
-    logging.info(f"Processing org {org_number}, report {report_id}")
+    logging.info(f"Processing org {org_number}, report {digitaliseringstiltak_report_id}")
 
     if regvil_instance_client.instance_created(
-        org_number, app_config.app_config.tag["tag_instance"]
+        org_number, api_config.app_config.tag["tag_instance"]
         ):
         logging.warning(
-                f"Skipping org {org_number} and report {report_id}- already in storage"
+                f"Skipping org {org_number} and report {digitaliseringstiltak_report_id}- already in storage"
             )
         return {
         "status": 200,
-        "report_id":report_id,
+        "report_id":digitaliseringstiltak_report_id,
         "org_number": org_number,
-        "app_name": app_config,
+        "app_name": api_config.app_config.app_name,
     }
     logging.info(
-            f"Creating new instance for org {org_number} and report id {report_id}"
+            f"Creating new instance for org {org_number} and report id {digitaliseringstiltak_report_id}"
         )
     
-    files = create_payload(org_number, dato, app_config, prefill_data)
+    files = create_payload(org_number, dato, api_config, prefill_data)
     created_instance = regvil_instance_client.post_new_instance(files)
-
     if created_instance.status_code == 201:
         instance_meta_data = created_instance.json()
+        party_id, instance_id = split_party_instance_id(instance_meta_data["id"])
         instance_data_meta_data = get_meta_data_info(
                 instance_meta_data.get("data")
             )
 
         logging.info(
-                f"Successfully created instance for org nr {org_number}/ report id {report_id}: {instance_meta_data['id']}"
+                f"Successfully created instance for org nr {org_number}/ report id {digitaliseringstiltak_report_id}: {instance_meta_data['id']}"
             )
         tracker.logging_instance(
-            instance_meta_data["id"],
+            instance_id,
             org_number,
-            report_id,
+            digitaliseringstiltak_report_id,
             instance_meta_data,
             instance_data_meta_data,
-            app_config.app_config.tag["tag_instance"],
+            api_config.app_config.tag["tag_instance"],
             )
         tracker.save_to_disk()
         tag_result = regvil_instance_client.tag_instance_data(
-                instance_meta_data["instanceOwner"]["partyId"],
-                instance_meta_data["id"],
+                party_id,
+                instance_id,
                 instance_data_meta_data["id"],
-                app_config.app_config.tag["tag_instance"],
+                api_config.app_config.tag["tag_instance"],
             )
         if tag_result.status_code == 201:
                 logging.info(f"Successfully tagged instance for org number: {org_number} party id: {instance_meta_data['instanceOwner']['partyId']} instance id: {instance_meta_data['id']}")
@@ -115,15 +119,15 @@ def run(org_number: str, report_id: str, dato: str, app_name: str, prefill_data:
         
         return {
         "status": 200,
-        "instance_id": instance_meta_data["id"],
+        "instance_id": instance_id,
         "party_id": instance_meta_data["instanceOwner"]["partyId"],
-        "report_id":report_id,
+        "digitaliseringstiltak_report_id":digitaliseringstiltak_report_id,
         "org_number": org_number,
-        "app_name": app_config,
+        "app_name": api_config.app_config.app_name,
     }
     else:
         logging.warning(
-                f"Failed to create instance for org nr {org_number}/ report id {report_id}: Status {created_instance.status_code}"
+                f"Failed to create instance for org nr {org_number}/ report id {digitaliseringstiltak_report_id}: Status {created_instance.status_code}"
             )
         try:
             error_details = created_instance.json()
@@ -134,7 +138,7 @@ def run(org_number: str, report_id: str, dato: str, app_name: str, prefill_data:
 
         logging.error(
                     f"API Error: Org {org_number}, "
-                    f"Report {report_id} - "
+                    f"Report {digitaliseringstiltak_report_id} - "
                     f"Status: {created_instance.status_code} - "
                     f"Error message: {error_msg}"
                 )
@@ -142,7 +146,7 @@ def run(org_number: str, report_id: str, dato: str, app_name: str, prefill_data:
         "status": 500,
         "instance_id": instance_meta_data["id"],
         "party_id": instance_meta_data["instanceOwner"]["partyId"],
-        "report_id":report_id,
+        "digitaliseringstiltak_report_id":digitaliseringstiltak_report_id,
         "org_number": org_number,
-        "app_name": app_config,
+        "app_name": api_config.app_config.app_name,
     }
