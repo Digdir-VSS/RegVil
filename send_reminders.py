179024fc189e9da325fe41de671232e52a467fd7
ignacio-digdir <ignacio.cuervo.torre@digdir.no>
2025-08-11 12:29:47 +0200

    added error logging if status is not 200 for send_reminders

--- send_reminders.py
+++ send_reminders.py
@@ -31,42 +31,49 @@ def main() -> None:
         for instance in instance_ids:
             partyID, instance_id = instance["instanceId"].split("/")
             instance = regvil_instance_client.get_instance(partyID, instance_id)
-            instance_meta = instance.json()
-            instance_data = instance_meta.get("data")
-            dataguid = get_meta_data_info(instance_data).get("id")
-            tag = get_meta_data_info(instance_data).get("tags")
-            visibleAfter = instance_meta.get("visibleAfter")
-            visibleAfterformated = datetime.fromisoformat(visibleAfter)
-            if instance_meta.get("isHardDeleted"):
-                logging.info(f"Instance {instance_id} is already hard deleted.")
-                continue
-            if instance_meta.get("isSoftDeleted"):
-                logging.warning(f"Instance {instance_id} is soft deleted.")
-                continue
-            if visibleAfterformated > datetime.now(timezone.utc):
-                logging.info(f"Instance {instance_id} is not yet visible.")
-                continue
-            if visibleAfterformated > datetime.now(timezone.utc) - timedelta(days=14):
-                logging.info(f"Instance {instance_id} is still within the 14-day visibility period.")
-                continue   
-            if len(tag) == 0 :
-                logging.info(f"Instance {instance_id} has no tag. Probalby deleted.")
-                continue
-            if instance_meta.get("status").get("createdBy") == instance_meta.get("status").get("lastChangedBy"):
-                data_response = regvil_instance_client.get_instance_data(partyID, instance_id, dataguid)
-                data = data_response.json()
-                already_sent = list_blobs_with_prefix(f"{os.getenv("ENV")}/varsling/{tag[0]}_{app}")
-                for blob in already_sent:
-                    json_file = read_blob(blob)
-                    if datetime.fromisoformat(json_file["sent_time"]) >datetime.now(timezone.utc) - timedelta(days=14):
+            if instance.status_code == 200:
+                instance_meta = instance.json()
+                instance_data = instance_meta.get("data")
+                dataguid = get_meta_data_info(instance_data).get("id")
+                tag = get_meta_data_info(instance_data).get("tags")
+                visibleAfter = instance_meta.get("visibleAfter")
+                visibleAfterformated = datetime.fromisoformat(visibleAfter)
+                if instance_meta.get("isHardDeleted"):
+                    logging.info(f"Instance {instance_id} is already hard deleted.")
+                    continue
+                if instance_meta.get("isSoftDeleted"):
+                    logging.warning(f"Instance {instance_id} is soft deleted.")
+                    continue
+                if visibleAfterformated > datetime.now(timezone.utc):
+                    logging.info(f"Instance {instance_id} is not yet visible.")
+                    continue
+                if visibleAfterformated > datetime.now(timezone.utc) - timedelta(days=14):
+                    logging.info(f"Instance {instance_id} is still within the 14-day visibility period.")
+                    continue   
+                if len(tag) == 0 :
+                    logging.info(f"Instance {instance_id} has no tag. Probalby deleted.")
+                    continue
+                if instance_meta.get("status").get("createdBy") == instance_meta.get("status").get("lastChangedBy"):
+                    data_response = regvil_instance_client.get_instance_data(partyID, instance_id, dataguid)
+                    if data_response.status_code == 200:
+                        data = data_response.json()
+                        already_sent = list_blobs_with_prefix(f"{os.getenv("ENV")}/varsling/{tag[0]}_{app}")
+                        for blob in already_sent:
+                            json_file = read_blob(blob)
+                            if datetime.fromisoformat(json_file["sent_time"]) >datetime.now(timezone.utc) - timedelta(days=14):
+                                continue
+                        org_number = data.get("Prefill").get("AnsvarligVirksomhet").get("Organisasjonsnummer")
+                        dato = datetime.now(timezone.utc).strftime("%Y-%m-%d")
+                        logging.info(f"Instance {instance_id} is created by the same user as last changed. Instance not answered.")
+                        file ={"org_number":org_number, "digitaliseringstiltak_report_id":tag[0], "dato":dato, "app_name":app, 
+                        "prefill_data":data}
+                        send_warning(**file)
+                    else:
+                        logging.error(f"Failed to get instance data for {instance_id} for party {partyID}: {data_response.text}")
                         continue
-                org_number = data.get("Prefill").get("AnsvarligVirksomhet").get("Organisasjonsnummer")
-                dato = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-                logging.info(f"Instance {instance_id} is created by the same user as last changed. Instance not answered.")
-                file ={"org_number":org_number, "digitaliseringstiltak_report_id":tag[0], "dato":dato, "app_name":app, 
-                "prefill_data":data}
-                send_warning(**file)
-
+            else:
+                logging.error(f"Failed to get instance {instance_id} for party {partyID}: {instance.text}")
+                continue
     return "All reminders have been sent."
 
 if __name__ == "__main__":
