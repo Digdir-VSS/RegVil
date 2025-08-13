1096713f4bb638e4cf46c3db8dfc17902cc5fdc1
matboe-94 <matthias.boeker@digdir.no>
2025-08-13 12:11:27 +0200

    remove print statement

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ from get_initiell_skjema import run as download_skjema
 from upload_single_skjema import run as upload_skjema
 from send_warning import run as send_notification
 from send_reminders import run as run_reminder_job
+
 load_dotenv()
 
 app = Flask(__name__)
+
 
 def extract_ids_from_source(source_url: str):
     parts = source_url.split("/")
@@ -29,34 +31,52 @@ def handle_event():
                 f"Party ID: {party_id}, Instance ID: {instance_id}, App name: {app_name}"
             )
             ## IF CLOUD EVENT
-            download_params, download_response = download_skjema(party_id=party_id, instance_id=instance_id, app_name=app_name)
+            download_params, download_response = download_skjema(
+                party_id=party_id, instance_id=instance_id, app_name=app_name
+            )
             if not download_params:
-                logging.error(f"Download failed for app name: {app_name} party id: {party_id} instance id: {instance_id}.")
-                return f"Download failed for app name: {app_name} party id: {party_id} instance id: {instance_id},", download_response
-            
+                logging.error(
+                    f"Download failed for app name: {app_name} party id: {party_id} instance id: {instance_id}."
+                )
+                return (
+                    f"Download failed for app name: {app_name} party id: {party_id} instance id: {instance_id},",
+                    download_response,
+                )
+
             if app_name == "regvil-2025-slutt":
-                logging.info(f"Terminal app reached: {app_name}. No further processing.")
+                logging.info(
+                    f"Terminal app reached: {app_name}. No further processing."
+                )
                 return "Workflow complete - no further action.", 200
 
             result = upload_skjema(**download_params)
             if result == 200:
                 notification_results = send_notification(**download_params)
                 if notification_results == 200:
-                    logging.info(f"Notification sent successfully for app name: {app_name} party id: {party_id} instance id: {instance_id}.")
-                    print(f"Notification sent successfully for app name: {app_name} party id: {party_id} instance id: {instance_id}.")
+                    logging.info(
+                        f"Notification sent successfully for app name: {app_name} party id: {party_id} instance id: {instance_id}."
+                    )
+                    print(
+                        f"Notification sent successfully for app name: {app_name} party id: {party_id} instance id: {instance_id}."
+                    )
                     return "Event received and processed. Notification sent", 200
                 else:
-                    logging.error(f"Notification failed for app name: {app_name} party id: {party_id} instance id: {instance_id}. Status code: {notification_results}")
-                    print(f"Notification failed for app name: {app_name} party id: {party_id} instance id: {instance_id}. Status code: {notification_results}")
+                    logging.error(
+                        f"Notification failed for app name: {app_name} party id: {party_id} instance id: {instance_id}. Status code: {notification_results}"
+                    )
+                    print(
+                        f"Notification failed for app name: {app_name} party id: {party_id} instance id: {instance_id}. Status code: {notification_results}"
+                    )
                     return "Event received and processed. Notification failed", 200
             else:
                 return "Error in processing", result
         else:
             logging.info("Event type not handled.")
-            return "Event type not handled", 204  
+            return "Event type not handled", 204
     except Exception as e:
         logging.error(f"Error: {e}")
         return f"Internal Server Error: {str(e)}", 500
+
 
 @app.route("/send_reminder", methods=["POST"])
 def send_reminder():
@@ -70,6 +90,7 @@ def send_reminder():
     except Exception as e:
         logging.exception("Error while processing send_reminder request")
         return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
