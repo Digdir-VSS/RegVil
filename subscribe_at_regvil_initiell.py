873cf76530cdfc9cb08499f4624950f8544d6b17
ignacio-digdir <ignacio.cuervo.torre@digdir.no>
2025-08-26 15:20:49 +0200

    prøvde med en ny branch å subscribe til prod

--- subscribe_at_regvil_initiell.py
+++ subscribe_at_regvil_initiell.py
@@ -8,11 +8,11 @@ from azure.keyvault.secrets import SecretClient
 
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url="https://keyvaultvss.vault.azure.net/", credential=credential)
-secret = client.get_secret("rapdigtest")
+secret = client.get_secret("regvilprod")
 secret_value = secret.value
 
 def subscribe_to_altinn_events(altinn_token: str, endpoint: str, source_filter: str, type_filter: str):
-    url = "https://platform.tt02.altinn.no/events/api/v1/subscriptions"
+    url = "https://platform.altinn.no/events/api/v1/subscriptions"
     headers = {
        "Authorization": f"Bearer {altinn_token}",
        "Content-Type": "application/json"
@@ -36,19 +36,20 @@ def main():
     maskinporten_endpoint = maskinporten_endpoints[test_config_client_file["environment"]]
 
     maskinport_client = {
-    "kid":"RapDig_maskinporten-TEST.2025-05-22",
-    "client_id":"387ff46c-222f-412b-ab63-3abf0a2704bc",
+    "kid":"regvil-app-TEST.2025-08-19",
+    "client_id":"c5e82235-7bab-496e-b28c-751566a6f810",
     "scope":"altinn:serviceowner altinn:events.subscribe"
 }
     token = exchange_token(maskinport_client ,secret_value,  maskinporten_endpoint)
 
     endpoint = "https://regvil-app.lemonforest-e288550d.norwayeast.azurecontainerapps.io/httppost"
-    source_filter = "https://digdir.apps.tt02.altinn.no/digdir/regvil-2025-initiell"
-    type_filter = "app.instance.process.completed"
-    response = subscribe_to_altinn_events(token, endpoint, source_filter, type_filter)
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
-    response.raise_for_status()
+    for source_filter in ["https://digdir.apps.altinn.no/digdir/regvil-2025-initiell", "https://digdir.apps.altinn.no/digdir/regvil-2025-oppstart", "https://digdir.apps.altinn.no/digdir/regvil-2025-status", "https://digdir.apps.altinn.no/digdir/regvil-2025-slutt"]:
+        type_filter = "app.instance.process.completed"
+        print(token)
+        response = subscribe_to_altinn_events(token, endpoint, source_filter, type_filter)
+        print("Status Code:", response.status_code)
+        print("Response:", response.text)
+        response.raise_for_status()
 
 if __name__ == "__main__":
     main()
