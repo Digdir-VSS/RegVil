cca3a388942c4b9d40d66c0532e5d410e26e43a8
matboe-94 <matthias.boeker@digdir.no>
2025-11-14 13:02:22 +0100

    new reinstate script

---
+++ reinstansiering.py
@@ -0,0 +1,80 @@
+import os 
+from pathlib import Path
+from datetime import datetime
+import pytz
+
+from clients.instance_client import AltinnInstanceClient, get_meta_data_info
+from config.config_loader import load_full_config
+
+from get_initiell_skjema import run as download_skjema
+from upload_single_skjema import run as upload_skjema
+from send_warning import run as send_notification
+
+def delete(regvil_instance_client: AltinnInstanceClient, partyID: str, instance_id: str):
+    print(f"Deleting instance {instance_id} for party {partyID}")
+    instance = regvil_instance_client.get_instance(partyID, instance_id)
+    instance_meta = instance.json()
+    instance_data = instance_meta.get("data")
+    dataguid = get_meta_data_info(instance_data).get("id")
+    tag = get_meta_data_info(instance_data).get("tags")
+    if tag:
+        tag_response = regvil_instance_client.delete_tag(partyID, instance_id, dataguid, tag[0])
+        print(tag_response.status_code)
+    instance_deleted = regvil_instance_client.delete_instance(partyID, instance_id)
+    if instance_deleted.status_code in [200,201,204]:
+        print(f"Successfully deleted instance {instance_id}")
+    else:
+        print(f"Failed to delete instance {instance_id}: {instance_deleted.text}")
+
+
+def reinstate(instance_id, party_id, app_name, isVisible):
+        print(
+                f"APP:Party ID: {party_id}, Instance ID: {instance_id}, App name: {app_name}"
+            )
+
+        download_params, download_response = download_skjema(
+                party_id=party_id, instance_id=instance_id, app_name=app_name
+            )
+        if not download_params:
+            print(
+                    f"APP:Download failed for app name: {app_name} party id: {party_id} instance id: {instance_id}."
+                )
+
+        if app_name == "regvil-2025-slutt":
+                print(
+                    f"APP:Terminal app reached: {app_name}. No further processing."
+                )
+
+        download_params["dato"] = isVisible
+        result = upload_skjema(**download_params)
+        if result == 200:
+                notification_results = send_notification(**download_params)
+
+                if notification_results == 200:
+                    print(
+                        f"APP:Notification sent successfully for app name: {app_name} party id: {party_id} instance id: {instance_id}."
+                    )
+                else:
+                    print(
+                        f"APP:Notification failed for app name: {app_name} party id: {party_id} instance id: {instance_id}. Status code: {notification_results}"
+                    )
+        else:
+            print("Error in processing", result)
+        
+
+def main():
+    path_to_config_folder = Path(__file__).parent / "config_files"
+
+    app_name = input("Enter the app name to delete the instance from: [regvil-2025-initiell, regvil-2025-oppstart, regvil-2025-status, regvil-2025-slutt]")
+    partyID = input("Please enter Party_id")
+    instance_id = input("Please enter a specific instance ID to delete:")
+    config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
+    regvil_instance_client = AltinnInstanceClient.init_from_config(
+        config,
+    )
+    isVisible = datetime.now(pytz.UTC)
+    reinstate(instance_id, partyID, app_name, isVisible)
+    delete(regvil_instance_client, partyID, instance_id)
+
+if __name__ == "__main__":
+     main()
