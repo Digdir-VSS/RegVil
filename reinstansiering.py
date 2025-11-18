f3df9d7f86ae03d4933a05602d2168ef8405b677
matboe-94 <matthias.boeker@digdir.no>
2025-11-18 12:24:54 +0100

    add cancel notification call

--- reinstansiering.py
+++ reinstansiering.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from datetime import datetime
 import pytz
 
+
+from clients.varsling_client import AltinnVarslingClient
 from clients.instance_client import AltinnInstanceClient, get_meta_data_info
 from config.config_loader import load_full_config
 
@@ -26,6 +28,16 @@ def delete(regvil_instance_client: AltinnInstanceClient, partyID: str, instance_
     else:
         print(f"Failed to delete instance {instance_id}: {instance_deleted.text}")
 
+def cancel_notification(varlsing_client: AltinnVarslingClient, notification_id: str):
+    response = varlsing_client.cancel_notification(notificantion_id=notification_id)
+    if response == 200:
+        print(f"Successfully cancelled notification!")
+
+    else:
+         
+        print(f"Notification not cancelled!")
+            
+     
 
 def reinstate(instance_id, party_id, app_name, isVisible):
         print(
@@ -65,13 +77,20 @@ def main():
     app_name = input("Enter the app name: [regvil-2025-initiell, regvil-2025-oppstart, regvil-2025-status, regvil-2025-slutt]  ")
     partyID = input("Please enter the Party Id: ")
     instance_id = input("Please enter a specific instance Id: ")
+    notification_id = input("Please enter a specific notification Id: ")
+    
     config = load_full_config(path_to_config_folder, app_name, os.getenv("ENV"))
     regvil_instance_client = AltinnInstanceClient.init_from_config(
+        config,
+    )
+    regvil_varsling_client = AltinnVarslingClient.init_from_config(
         config,
     )
     VisibleAfter =  datetime.now(pytz.UTC).isoformat().replace('+00:00', 'Z') #, "visibleAfter": "2019-05-20T00:00:00Z" 
     reinstate(instance_id, partyID, app_name, VisibleAfter)
     delete(regvil_instance_client, partyID, instance_id)
+    cancel_notification(regvil_varsling_client, notification_id)
+
 
 if __name__ == "__main__":
      main()
